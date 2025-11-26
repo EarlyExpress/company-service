fc6e02f60e4ea3314feda3fc69d613a1eea6e784
강동현 <peterkang98@gmail.com>
2025-11-26 15:46:34 +0900

    feat: 업체 엔티티 생성

---
+++ src/main/java/com/early_express/company_service/domain/entity/Company.java
@@ -0,0 +1,48 @@
+package com.early_express.company_service.domain.entity;
+
+import com.early_express.company_service.domain.vo.Type;
+import com.early_express.company_service.global.common.utils.UuidUtils;
+import com.early_express.company_service.global.infrastructure.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(
+	name = "p_customer",
+	indexes = {
+		@Index(name = "idx_company_name", columnList = "name"),
+		@Index(name = "idx_company_owner_hub", columnList = "ownerHubId")
+	}
+)
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Company extends BaseEntity {
+	@Id
+	@Column(length = 36, updatable = false, nullable = false)
+	private String id;
+
+	@Column(nullable = false)
+	private String name;
+
+	@Enumerated(EnumType.STRING)
+	@Column(length = 6)
+	private Type type;
+
+	@Column(length = 36)
+	private String ownerHubId;
+
+	@Column(length = 100)
+	private String address;
+
+	@Builder
+	public Company(String name, Type type, String ownerHubId, String address) {
+		this.id = UuidUtils.generate();
+		this.name = name;
+		this.type = type;
+		this.ownerHubId = ownerHubId;
+		this.address = address;
+	}
+}
